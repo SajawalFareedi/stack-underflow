cd5cde3606fef49dcd5888c6fd3f93418762e19a
SajawalFareedi <sajawalfareedi448@gmail.com>
2023-02-02 15:56:28 +0500

    Still working on homepage

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <nav className={`${inter.className} ${styles.mainnav}`}>
         <ul>
           <li>Home</li>
-          <li>Blog</li>
+          <li>Questions</li>
           <li>About</li>
           <li>Support</li>
         </ul>
@@ -28,22 +28,39 @@ export default function Home() {
       <main className={`${inter.className} ${styles.main}`}>
         <div className={styles.main__heading}>
           <h1>Stack Underflow</h1>
-          <code className={styles.code}>By a dev for devs</code>
+          <code className={styles.code}>One problem, multiple solutions</code>
         </div>
 
         <div className={styles.blogs}>
-          <h2 className={styles.pop__blog_head}>Popular blogs</h2>
+          <h2 className={styles.pop__blog_head}>Top Questions</h2>
           <div className={styles.blogItem}>
-            <h3>A popular blog by author</h3>
-            <p>This is a popluar blog of 2023. Do check it out</p>
+            <h3>
+              Why is processing a sorted array faster than processing an
+              unsorted array?
+            </h3>
+            <p>
+              Here is a piece of C++ code that shows some very peculiar
+              behavior. For some reason, sorting the data (before the timed
+              region) miraculously makes the primary loop almost six times
+              faster: #include &...
+            </p>
           </div>
           <div className={styles.blogItem}>
-            <h3>A popular blog by author</h3>
-            <p>This is a popluar blog of 2023. Do check it out</p>
+            <h3>Which JSON content type do I use?</h3>
+            <p>
+              There are many &quot;standards&quot; for the JSON content type:
+              application/json application/x-javascript text/javascript
+              text/x-javascript text/x-json Which one do I use, and where? I
+              assume ...
+            </p>
           </div>
           <div className={styles.blogItem}>
-            <h3>A popular blog by author</h3>
-            <p>This is a popluar blog of 2023. Do check it out</p>
+            <h3>How do I undo the most recent local commits in Git?</h3>
+            <p>
+              I accidentally committed the wrong files to Git, but didn't push
+              the commit to the server yet. How do I undo those commits from the
+              local repository?
+            </p>
           </div>
         </div>
       </main>
